b872ea3f3e9af1c03d8b9abf87da6e03cf24dab8
RiveraDenisse <denisse_alejandrar@hotmail.com>
2021-10-02 22:48:21 -0500

    added schemas

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,8 +2,42 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+  type Book {
+    _id: ID
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
   type Query {
-    helloWorld: String
+    me: [User]
+  }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(book: SavedBookInput): User
+    removeBook(bookId: String!): User
+  }
+  input SavedBookInput {
+    authors: [String]
+    description: String
+    bookId: String
+    image: String
+    link: String
+    title: String
   }
 `;
 

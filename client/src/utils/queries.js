fba1baaba9503a81d2fcf821e31c3d90dec3358d
RiveraDenisse <denisse_alejandrar@hotmail.com>
2021-10-03 12:58:47 -0500

    create queries.js file

---
+++ client/src/utils/queries.js
@@ -0,0 +1,22 @@
+//this will store all of the GraphQL query requests
+
+import { gql } from "@apollo/client";
+
+export const GET_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;

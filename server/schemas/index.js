181c92cb3d03c21d8767a2c90292cb7f0c2a240b
RiveraDenisse <denisse_alejandrar@hotmail.com>
2021-10-02 20:39:36 -0500

    implement apollo server and middleware

---
+++ server/schemas/index.js
@@ -0,0 +1,8 @@
+const typeDefs = require("./typeDefs");
+const resolvers = require("./resolvers");
+
+//create typeDefs
+// const typeDefs = gql``;
+
+//export typeDefs
+module.exports = { typeDefs, resolvers };

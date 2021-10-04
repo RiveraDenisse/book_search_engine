e518fc2b8b3d532ab52194e3bf896fb8db5668a8
RiveraDenisse <denisse_alejandrar@hotmail.com>
2021-10-03 19:25:35 -0500

    added save_book,add_user,login_user and remove_user to mutations

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -7,14 +7,16 @@ import {
   Button,
 } from "react-bootstrap";
 import { useParams } from "react-router-dom";
-import { useQuery } from "@apollo/client";
+import { useQuery, useMutation } from "@apollo/client";
 import { GET_ME } from "../utils/queries";
+
 import { getMe, deleteBook } from "../utils/API";
 import Auth from "../utils/auth";
 import { removeBookId } from "../utils/localStorage";
 
 const SavedBooks = () => {
   const { loading, data } = useQuery(GET_ME);
+
   const userData = data?.me || {};
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database

0531b9a2a4015ea8a5a16aeaad0d6bfa2f509454
RiveraDenisse <denisse_alejandrar@hotmail.com>
2021-10-03 20:43:08 -0500

    fixed signup, modified auth.js

--- server/schemas/index.js
+++ server/schemas/index.js
@@ -1,8 +1,5 @@
 const typeDefs = require("./typeDefs");
 const resolvers = require("./resolvers");
 
-//create typeDefs
-// const typeDefs = gql``;
-
 //export typeDefs
 module.exports = { typeDefs, resolvers };
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -4,36 +4,35 @@ const jwt = require("jsonwebtoken");
 const secret = "mysecretsshhhhh";
 const expiration = "2h";
 
-module.exports = {
-  // function for our authenticated routes
-  authMiddleware: function ({ req }) {
-    // allows token to be sent via  req.query or headers
-    let token = req.body.token || req.query.token || req.headers.authorization;
+// Apollo Server Authentication
 
-    // ["Bearer", "<tokenvalue>"]
+module.exports = {
+  signToken: function ({ username, email, _id }) {
+    // create a token with the username, email, and id, signed with the secret
+    const payload = { username, email, _id };
+
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+  authMiddleware: function ({ req }) {
+    // token may be sent via req.body, req.query, or headers
+    let token = req.body.token || req.query.token || req.headers.authorization;
+    // if by headers, remove "Bearer" from "<tokenvalue>" value and return just the token value
     if (req.headers.authorization) {
       token = token.split(" ").pop().trim();
     }
-    //if no token, return request object as is
+    // if there is no token, return the request object as sent
     if (!token) {
       return req;
     }
-
-    // verify token and get user data out of it
+    // otherwise...
     try {
-      //decode and attach user data to request object
+      // decode and attach user data to the request object
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
       console.log("Invalid token");
     }
+    // return the updated request object
     return req;
-    // send to next endpoint
-    next();
   },
-  //   signToken: function ({ username, email, _id }) {
-  //     const payload = { username, email, _id };
-
-  //     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-  //   },
 };
